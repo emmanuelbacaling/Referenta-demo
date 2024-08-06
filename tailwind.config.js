89af0872b243e52ac18c2b8dbbe67e88dbc1316e
Emmanuel Bacaling <emmanb@2damcreative.com>
2024-08-07 00:16:17 +0800

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,18 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     colors: {
-      primary: "#1b8f75",
+      primary: {
+        DEFAULT: "#1c8f75",
+        900: "#3f9a82",
+        800: "#58a58f"
+      },
       white: "#fff",
       disabled: "#cccfd1",
       black: "#1a262f",
-      success: "#4BB543"
+      success: {
+        DEFAULT: "#198754",
+        700: "#6bab85"
+      }
     },
     extend: {}
   },
